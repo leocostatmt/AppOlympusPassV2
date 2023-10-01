b53568a5fbbe30ca8a2193f26ec098acfc546547
leocostatmt <leonardocaetanocosta2005@gmail.com>
2023-10-01 14:58:55 -0300

    Changes to be committed: 	new file:   .dart_tool/dartpad/web_plugin_registrant.dart 	new file:   .dart_tool/package_config.json 	new file:   .dart_tool/package_config_subset 	new file:   .dart_tool/version 	new file:   .flutter-plugins 	new file:   .flutter-plugins-dependencies 	modified:   README.md 	new file:   build/99e43e293816255c3ccce3e83f7937f4/_composite.stamp 	new file:   build/99e43e293816255c3ccce3e83f7937f4/gen_dart_plugin_registrant.stamp 	new file:   build/99e43e293816255c3ccce3e83f7937f4/gen_localizations.stamp 	new file:   build/ade00fc64fa695e0daed02c1105f6d67.cache.dill.track.dill 	new file:   build/flutter_assets/AssetManifest.bin 	new file:   build/flutter_assets/AssetManifest.json 	new file:   build/flutter_assets/FontManifest.json 	new file:   build/flutter_assets/NOTICES 	new file:   build/flutter_assets/assets/audios/favicon.png 	new file:   build/flutter_assets/assets/fonts/Rubik-Regular.ttf 	new file:   build/flutter_assets/assets/fonts/favicon.png 	new file:   build/flutter_assets/assets/images/6670fd9d-b57a-486b-a4a7-e124e9056a71.jpg 	new file:   build/flutter_assets/assets/images/Splash_Olympus_Pass.gif 	new file:   build/flutter_assets/assets/images/Splash_Olympus_Pass_2-removebg-preview.png 	new file:   build/flutter_assets/assets/images/app_launcher_icon.png 	new file:   build/flutter_assets/assets/images/bg_login@2x.png 	new file:   build/flutter_assets/assets/images/favicon.png 	new file:   build/flutter_assets/assets/lottie_animations/favicon.png 	new file:   build/flutter_assets/assets/pdfs/favicon.png 	new file:   build/flutter_assets/assets/rive_animations/favicon.png 	new file:   build/flutter_assets/assets/videos/favicon.png 	new file:   build/flutter_assets/fonts/MaterialIcons-Regular.otf 	new file:   build/flutter_assets/packages/cupertino_icons/assets/CupertinoIcons.ttf 	new file:   build/flutter_assets/packages/font_awesome_flutter/lib/fonts/fa-brands-400.ttf 	new file:   build/flutter_assets/packages/font_awesome_flutter/lib/fonts/fa-regular-400.ttf 	new file:   build/flutter_assets/packages/font_awesome_flutter/lib/fonts/fa-solid-900.ttf 	new file:   build/flutter_assets/shaders/ink_sparkle.frag 	modified:   macos/Flutter/GeneratedPluginRegistrant.swift 	new file:   pubspec.lock 	modified:   windows/flutter/generated_plugin_registrant.cc 	modified:   windows/flutter/generated_plugins.cmake

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,12 +5,32 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_macos
+import cloud_firestore
+import device_info_plus
+import file_selector_macos
+import firebase_auth
+import firebase_core
+import firebase_storage
+import geolocator_apple
+import path_provider_foundation
+import pdfx
+import shared_preferences_macos
+import sign_in_with_apple
 import sqflite
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  FLTFirebaseFirestorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseFirestorePlugin"))
+  DeviceInfoPlusMacosPlugin.register(with: registry.registrar(forPlugin: "DeviceInfoPlusMacosPlugin"))
+  FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  FLTFirebaseAuthPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAuthPlugin"))
+  FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
+  FLTFirebaseStoragePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseStoragePlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  PdfxPlugin.register(with: registry.registrar(forPlugin: "PdfxPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SignInWithApplePlugin.register(with: registry.registrar(forPlugin: "SignInWithApplePlugin"))
   SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
